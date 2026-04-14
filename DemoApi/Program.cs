563045cc5d1a024e405a050552db2e16e7b7cb99
Ravindra Negi <ravindra.negi42@outlook.com>
2026-04-14 16:09:22 +0530

    intial commit

---
+++ DemoApi/Program.cs
@@ -0,0 +1,20 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+app.MapGet("/api/hello", () =>
+{
+    return new { message = "Hello from .NET API 🚀" };
+});
+
+app.Run();
